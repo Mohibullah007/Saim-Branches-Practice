0938331ebd5333b9da2091d032870bc931734fbe
Mohibullah007 <hah.poP.1995>
2022-04-04 03:37:04 -0400

    item update assigned for Store

--- src/Store.java
+++ src/Store.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Store {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter items you need from the above");
     }
 }
