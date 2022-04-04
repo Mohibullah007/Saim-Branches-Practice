1db7afdac8fde1716e4928093fcaf15c67a1ef90
Mohibullah007 <hah.poP.1995>
2022-04-04 03:22:13 -0400

    changes assigned in water

--- src/Water.java
+++ src/Water.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Water {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a water type");
+        System.out.println("Enter a water from the above List\n" +
+                "1)\t~\tMineral water\n" +
+                "2)\t~\tPurified Water");
         String water = input.nextLine();
     }
 }
