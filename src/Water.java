05db896d0ba822ea2d302535738c74f7ba231d7a
Mohibullah007 <hah.poP.1995>
2022-04-04 03:00:42 -0400

    Scanner for water type assigned

---
+++ src/Water.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Water {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a water type");
+        String water = input.nextLine();
+    }
+}
